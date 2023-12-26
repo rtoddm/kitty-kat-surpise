0464253488cbc1dd3e1fd5911a5f734a338500f6
ToddM <rtoddm@gmail.com>
2023-12-26 15:34:56 -0600

    Added if/else statement in JS to toggle picture on/off

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 const button = document.querySelector(".show-cat");
 const cat = document.querySelector(".cat");
 
+// button.addEventListener("click", function () {
+//   cat.classList.add("show");
+// });
+
 button.addEventListener("click", function () {
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Click here for a surprise!";
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Make the kitty cat disappear!";
+  }
 });
